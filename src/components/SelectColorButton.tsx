d5ac272025021d25c5d2558f0c3df770dc1cb8cf
Dexshine <b_o_y_s@hotmail.com>
2021-09-04 23:32:36 +0700

    highlight color picker

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,18 +1,23 @@
+import { changeColor, PixelPainterStore } from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
 }
 
 const SelectColorButton = ({ color }: SelectColorButtonProps) => {
 
+  const state = PixelPainterStore.useState()
+
   //modify this function to highlight correctly
   const computeRingSize = () => {
-    return ""
-    return "ring-8 ring-green-400"
+    if(state.color === color) return "ring-8 ring-green-400"
+    else return ""
   }
 
   return (
-    <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
-      style={{ backgroundColor: color }}
+    <div  onClick={() => changeColor(color)} 
+          className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
+          style={{ backgroundColor: color }}
     >
     </div>
   )
