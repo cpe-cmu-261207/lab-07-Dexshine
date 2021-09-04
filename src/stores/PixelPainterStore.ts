6034478956edf61a129170ba69c15eb3f7ddbcab
Dexshine <b_o_y_s@hotmail.com>
2021-09-05 00:28:53 +0700

    add randomCanvas function

--- src/stores/PixelPainterStore.ts
+++ src/stores/PixelPainterStore.ts
@@ -23,21 +23,82 @@ export const PixelPainterStore = new Store<PixelPainterStoreType>({
   color: '#FFFFFF'
 })
 
+//change state color to the same as color from colorpicker
 export const changeColor = (color: string) => {
   PixelPainterStore.update(
     state => {state.color = color}
   )
 }
 
+//change color in cell(x, y) to the same as state color
 export const clickCell = (x:number, y:number) => {
   PixelPainterStore.update(
     state => {state.canvas[x][y] = state.color}
   )
 }
 
+//change color in all cell to white
 export const clearCanvas = () => {
   PixelPainterStore.update(
     state => {state.canvas = createEmptyCanvas()}
+  )
+}
+
+//change color in all cell by random
+export const randomCanvas = () => {
+  
+  const output: string[][] = []
+  for (let i=0; i<16; i++){
+    output[i] = []
+    for (let j=0; j<16; j++){
+      //random integer from 1 to 12
+      const randomNum = Math.floor(Math.random() * 12) + 1
+      let colorCode = ""
+      //change colorCode by switch-case
+      switch(randomNum){
+        case 1:
+          colorCode = "#000000"
+          break
+        case 2:
+          colorCode = "#804000"
+          break
+        case 3:
+          colorCode = "#FE0000"
+          break
+        case 4:
+          colorCode = "#FE6A00"
+          break
+        case 5:
+          colorCode = "#FFD800"
+          break
+        case 6:
+          colorCode = "#00FF01"
+          break
+        case 7:
+          colorCode = "#FFFFFF"
+          break
+        case 8:
+          colorCode = "#01FFFF"
+          break
+        case 9:
+          colorCode = "#0094FE"
+          break
+        case 10:
+          colorCode = "#0026FF"
+          break
+        case 11:
+          colorCode = "#B100FE"
+          break
+        case 12:
+          colorCode = "#FF006E"
+          break
+      }
+      output[i].push(colorCode)
+    }
+  }
+  
+  PixelPainterStore.update(
+    state => {state.canvas = output}
   )
 }
 
